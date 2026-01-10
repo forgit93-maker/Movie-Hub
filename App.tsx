0fda8eb3be1a04b5a044486f2d67c1f41e36de23
forgit93-maker <forgit93@gmail.com>
2026-01-10 20:31:31 +0530

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { StoreProvider } from './context/StoreContext';
 import { LanguageProvider } from './context/LanguageContext';
+import { HelmetProvider } from 'react-helmet-async';
 import Navbar from './components/Navbar';
 import BottomNav from './components/BottomNav';
 import Footer from './components/Footer';
@@ -31,49 +32,51 @@ function App() {
   }, []);
 
   return (
-    <LanguageProvider>
-      <StoreProvider>
-        <Router>
-          {/* Global wrapper with Theme-based Background and Text colors */}
-          <div className="min-h-screen font-sans transition-colors duration-300 bg-white text-gray-900 dark:bg-black dark:text-white flex flex-col">
-            <Navbar />
-            
-            <main className="flex-grow">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/movies" element={<Movies />} /> 
-                <Route path="/tv" element={<TVSeries />} /> 
-                <Route path="/details/:type/:id" element={<Details />} />
-                <Route path="/search" element={<Search />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/blog" element={<Blog />} />
-                
-                {/* Protected Routes - Split into Favorites (Watchlist) and Account (Profile) */}
-                <Route 
-                  path="/profile" 
-                  element={
-                    <ProtectedRoute>
-                      <Account />
-                    </ProtectedRoute>
-                  } 
-                />
-                <Route 
-                  path="/watchlist" 
-                  element={
-                    <ProtectedRoute>
-                      <Favorites />
-                    </ProtectedRoute>
-                  } 
-                />
-              </Routes>
-            </main>
-            
-            <BottomNav />
-            <Footer />
-          </div>
-        </Router>
-      </StoreProvider>
-    </LanguageProvider>
+    <HelmetProvider>
+      <LanguageProvider>
+        <StoreProvider>
+          <Router>
+            {/* Global wrapper with Theme-based Background and Text colors */}
+            <div className="min-h-screen font-sans transition-colors duration-300 bg-white text-gray-900 dark:bg-black dark:text-white flex flex-col">
+              <Navbar />
+              
+              <main className="flex-grow">
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/movies" element={<Movies />} /> 
+                  <Route path="/tv" element={<TVSeries />} /> 
+                  <Route path="/details/:type/:id" element={<Details />} />
+                  <Route path="/search" element={<Search />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/blog" element={<Blog />} />
+                  
+                  {/* Protected Routes - Split into Favorites (Watchlist) and Account (Profile) */}
+                  <Route 
+                    path="/profile" 
+                    element={
+                      <ProtectedRoute>
+                        <Account />
+                      </ProtectedRoute>
+                    } 
+                  />
+                  <Route 
+                    path="/watchlist" 
+                    element={
+                      <ProtectedRoute>
+                        <Favorites />
+                      </ProtectedRoute>
+                    } 
+                  />
+                </Routes>
+              </main>
+              
+              <BottomNav />
+              <Footer />
+            </div>
+          </Router>
+        </StoreProvider>
+      </LanguageProvider>
+    </HelmetProvider>
   );
 }
 
